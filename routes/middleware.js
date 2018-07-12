9bd627432ecd4587d5cb3e54876d64ffeb2dbabe
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-07-12 06:20:00 +0100

    dev

--- routes/middleware.js
+++ routes/middleware.js
@@ -23,7 +23,8 @@ var _ = require('lodash');
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' }
+		{ label: 'Blog', key: 'blog', href: '/blog' },
+		// { label: 'Register', key: 'register', href: '/register' }
 		// ,
 		// { label: 'Announcement', key: 'announcement', href: '/announcement' }
 	];
